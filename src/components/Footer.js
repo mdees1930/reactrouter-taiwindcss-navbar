befd20c45f8f295557378f4538ec4ee93f7131e1
Madsan <madsan.2509@gmail.com>
2022-11-12 20:40:15 +0700

    add hamburger menu responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,9 @@ export default function Footer(){
     return(
       <footer className="flex bottom-0 relative w-full p-5 justify-center items-center font-poppins bg-slate-900 text-white " >
         <small>©2022 madsan development. All rights reserved.</small>
+        {/* <div className="flex flex-col text-2xl text-white cursor-pointer">
+          <ion-icon name="menu"></ion-icon>
+        </div> */}
       </footer>
     )
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Menu from '../images/menu-outline.svg';
+import Close from '../images/close-circle-outline.svg';
 import Logo from '../images/react-logo.png';
 import { Link } from "react-router-dom";
+// import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-
+//border
+// border-solid border-2 border-red-700
 
 export default function Header(){
+  let [open, setOpen] = useState(false);
     return (
+      
       <header>
         <div>
-          <nav className ="flex fixed z-40 w-full justify-between px-5 py-2 content-center items-center bg-slate-900">
-            <div className = "flex w-7/12">
-              <img src={Logo} alt="React logo" className ="flex w-8 h-8 sm:h-8 sm:w-8 md:h-10 md:w-10 items-center content-center justify-center"/>
-              <span className="flex font-lobster justify-self-center items-center w-1/4 font-bold text-2xl sm:text-3xl md:text-3xl lg:text-4xl xl:4xl text-white">ReactTool</span>
+          <nav className =" flex flex-col py-2 md:py-1 fixed z-40 items-left w-full bg-slate-900 md:flex md:flex-row md:justify-between md:items-center">
+            
+            <div className = "flex w-7/12 mb-50 md:pl-5 pl-5">
+              <img src={Logo} alt="React logo" className =" flex w-10 h-10 sm:h-10 sm:w-10 md:h-12 md:w-12 items-center content-center justify-center"/>
+              <span className=" flex font-lobster justify-self-center items-center w-full font-bold text-3xl sm:text-3xl md:text-3xl lg:text-4xl text-white">ReactTool</span>
             </div>   
-            <ul className= "flex font-poppins items-center justify-self-center text-sm sm:text-sm md:text-lg lg:text-lg xl:text-lg text-white">
-              <li >
-                <Link to="/" className=" p-3">Home</Link>
+            
+            
+            <ul className= {`flex flex-col-reverse items-center md:bg-slate-900 z-[-1] md:z-auto left-0 w-full md:-w-auto pl-0 md:flex md:flex-row-reverse md:items-center md:pb-0 pb-2 md:pr-5 absolute md:static transition-all duration-500 ease-in text-sm sm:text-sm md:text-sm lg:text-lg font-poppins text-white ${open ? 'top-10 opacity-100 bg-slate-900 bg-opacity-80' : 'top-[-490px]'} md:opacity-100 opacity-100`}>
+              
+              <li className="p-5 md:p-2 md:flex md:justify-between">
+                <Link to="/contact" className=" px-5 hover:text-gray-400 duration-500">CONTACT</Link>
               </li>
-              <li >
-                <Link to="/pricing" className=" p-3">Pricing</Link>
+              <li className="p-5 md:p-2 md:flex md:justify-between">
+                <Link to="/about" className=" px-5 hover:text-gray-400 duration-500">ABOUT</Link>
               </li>
-              <li>
-                <Link to="/about" className=" p-3">About</Link>
+              <li className="p-5 md:p-2 md:flex md:justify-between">
+                <Link to="/pricing" className=" px-5 hover:text-gray-400 duration-500">PRICING</Link>
               </li>
-              <li>
-                <Link to="/contact" className=" p-3">Contact</Link>
+              <li className="pt-10 p-5 md:p-2 md:flex md:justify-between">
+                <Link to="/" className=" px-5 hover:text-gray-400 duration-500">HOME</Link>
               </li>
             </ul>
+
+            <div onClick={()=>setOpen(!open)}>
+              <img src={open ? Close : Menu} alt="MENU" className="flex mx-5 my-2 bg-white rounded-xl absolute top-0 right-0 w-8 h-8 cursor-pointer text-white text-md md:hidden"/>
+            </div>
+
           </nav>  
-          
         </div>
       </header>  
     );
